fb35c7f28c7b8620eb57fe23e8e7ab23de72774c
carolinashima <shima.carolina@gmail.com>
2026-06-13 22:40:09 +0200

    add multilang

--- pages/2_Lista.py
+++ pages/2_Lista.py
@@ -4,14 +4,10 @@ import datetime
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+from translations import translations
+lang = st.session_state.get("lang", "es")
+t = translations[lang]
 
-# page config
-st.set_page_config(layout='wide',
-initial_sidebar_state='expanded',
-page_title='Heladito map',
-page_icon='🍦')
-
-st.sidebar.header('🍦 Heladito map')
 st.sidebar.markdown('''
 ---
 Made by Carolina L. Shimabukuro 👩🏻‍💻
@@ -25,34 +21,35 @@ social_media_icons = SocialMediaIcons(social_media_links, colors)
 
 social_media_icons.render(sidebar=True, justify_content='start')
 
-st.header('Lista completa')
+if lang == 'es':
+    rev_col_name = 'review'
+elif lang == 'en':
+    rev_col_name = 'review_en'
 
 selected_cols = ['fecha',
 'nombre',
 'local',
 'gusto',
 'rating',
-'review',
 'notas',
 'tipo',
 'category',
 'base',
-'cositos']
+'cositos',
+rev_col_name]
 
 df_shops = st.session_state['df_shops']
 df_tracker = st.session_state['df_tracker']
-st.subheader("Heladerías")
-st.write("""
-Seleccioná una heladería para ver qué gustos pedí y qué me pareció en general.
-""")
+st.subheader(t["2_subh1"])
+st.write(t["2_text"])
 
 # select city
-selected_city = st.selectbox("Ciudad", df_shops['city'].unique())
+selected_city = st.selectbox(t["2_cityselect"], df_shops['city'].unique())
 df_city = df_shops[df_shops['city'] == selected_city].copy()
 
 # select shop
 df_city['display_name'] = df_city['nombre'] + " (" + df_city['local'] + ")"
-selected_shop = st.selectbox("Heladería", df_city['display_name'].sort_values())
+selected_shop = st.selectbox(t["2_shopselect"], df_city['display_name'].sort_values())
 
 shop_row = df_city[df_city['display_name'] == selected_shop].iloc[0]
 
@@ -74,16 +71,15 @@ flavor_avg = (
 
 # shop
 st.markdown(f"### {shop_row['nombre']}")
-st.caption(f"{shop_row['local']} · Promedio: **{shop_row['avg']:.1f} / 5**")
-#if pd.notna(shop_row['notas']) and shop_row['notas'] != '':
+st.caption(f'''{shop_row['local']} · {t["2_shopavg"]}: **{shop_row['avg']:.1f} / 5**''')
 if pd.notna(shop_row.get('notas', '')) and shop_row['notas'] != '':
-    st.write(f"Notas: {shop_row['notas']}")
+    st.write(f'''{t["2_notes"]}: {shop_row['notas']}''')
 
 
 for _, f in flavor_avg.iterrows():
     estrellas = "★" * round(f['avg_rating']) + "☆" * (5 - round(f['avg_rating']))
     
-    # Get all entries for this flavor, sorted newest first
+    # get all entries for this flavor, sorted newest first
     entradas = (
         sabores[sabores['gusto'] == f['gusto']]
         .sort_values('fecha', ascending=False)
@@ -91,8 +87,8 @@ for _, f in flavor_avg.iterrows():
     
     reviews_html = ""
     for _, e in entradas.iterrows():
-        if pd.notna(e['review']) and e['review'] != '':
-            reviews_html += f"<p style='margin:2px 0; font-size:12px; color:#888;'><b>{e['fecha']}</b>: {e['review']}</p>"
+        if pd.notna(e[rev_col_name]) and e[rev_col_name] != '':
+            reviews_html += f"<p style='margin:2px 0; font-size:12px; color:#888;'><b>{e['fecha']}</b>: {e[rev_col_name]}</p>"
     
     st.markdown(f"""
     <div style="padding:8px 0; border-bottom:0.5px solid #eee;">
@@ -106,17 +102,17 @@ for _, f in flavor_avg.iterrows():
 
 st.divider()
 
-st.header('Lista entera')
+st.header(t["2_subh2"])
 sorted_df = st.session_state['df_tracker'][selected_cols].sort_values(by='fecha', ascending = False)
-with st.expander('Ver tabla'):
+with st.expander(t["2_tabletitle"]):
     st.dataframe(sorted_df, hide_index=True,
-    column_order = ('fecha','nombre','local','gusto','rating','review','notas'),
+    column_order = ('fecha','nombre','local','gusto','rating',rev_col_name,'notas'),
     column_config = {
         'fecha': 'Fecha',
         'nombre': 'Nombre',
         'local': 'Local',
         'gusto': 'Gusto',
         'rating': 'Rating',
-        'review': 'Review',
+        rev_col_name: 'Review',
         'notas': 'Notas'
         })
