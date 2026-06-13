1bc648d86a2806b85c0929a8d5ec32bae154579f
carolinashima <shima.carolina@gmail.com>
2026-06-13 22:40:44 +0200

    multilang dictionary (es, en)

---
+++ translations.py
@@ -0,0 +1,158 @@
+translations = {
+    "es": {
+        # main page
+        #"0_title": "Heladito map",
+        "0_subh": "Tracker de helados que voy probando por ahí.",
+        "0_text": """
+            Hola! Hice esta pequeña app para visualizar mejor las heladerías
+            y helados que voy probando por ahí, como buena #GordaHeladitos.
+
+            Si tenés ganas de recomendarme alguna heladería llená el formulario en
+            <a href="{app_path}/{page}" target="_self">Recomendame</a>
+            y agregaré tu sugerencia a mi mapita. Gracias! 🙌""",
+        "0_imagecapt": "El sueño de la piba: helado gigante 🤩. Massera en algún lugar de la costa atlántica, 15 de febrero de 1995.",
+
+        # map page
+        "1_title": "Mapa",
+        "1_maintext": """
+            Hacé click en el marker para ver el puntaje promedio de la heladería.
+            Los íconos están color-coded según el puntaje promedio y mis estándares heladeriles:  
+            🟥: simplemente una gaver  
+            🟧: preferiría clavar un Torpedo en el kiosco  
+            🟦: bastante bien, si estoy de paso me pido un cuarto  
+            🟩: inyéctenlo en mis venasss
+        """,
+        "1_cityopt": "Ciudad",
+        "1_show": "Mostrar",
+        "1_showopt1": "Todos",
+        "1_showopt2": "Visitados",
+        "1_showopt3": "Recomendados",
+
+        # list page
+        "2_title": "Lista completa",
+        "2_subh1": "Heladerías",
+        "2_text": "Seleccioná una heladería para ver qué gustos pedí y qué me pareció en general.",
+        "2_cityselect": "Ciudad",
+        "2_shopselect": "Heladería",
+        "2_shopavg": "Promedio",
+        "2_subh2": "Lista entera",
+        "2_tabletitle": "Ver tabla",
+
+        # stats page
+        "3_text1": "Cuáles son mis gustos favoritos? Cuáles son los que más pido? La respuesta no los sorprenderá.",
+        "3_yearselect": "Seleccioná un año:",
+        "3_yearselect1": "Todos",
+        "3_text2": "A cuántas heladerías (sucursales agrupadas) fui y cuántos gustos diferentes pedí?",
+        "3_nshops": "N° heladerías",
+        "3_nflavours": "N° gustos",
+        "3_rank_title": "Ranking de gustos",
+        "3_rank_text": "Cuáles fueron mis gustos favoritos? A igual puntaje, el orden es random!",
+        "3_scoredistr": "Distribución de puntajes",
+        "3_scoredistr_text": "Del 1 al 5",
+        "3_creamsorbet": "A la crema o al agua?",
+        "3_category": "Categoría",
+        "3_category_text": "Categorías típicas de heladería.",
+        "3_baseflavour": "Sabor base",
+        "3_baseflavour_text": "El sabor con mayor porcentaje (a ojo) del gusto en cuestión. Algunos raris los pedí solo 1 vez así que solo muestro el top 15.",
+        "3_extrastoppings": "Cositos extra dentro o sobre la crema",
+        "3_extrastoppings_text": "Puede ser que más de 1 fuera un mix. Solo el top 10!",
+
+        # recommend page
+        "4_title": "Recomendame algo!",
+        "4_maintext": """
+            Si conocés alguna heladería/gustos que debería probar, porfa llená el formulario así lo tengo en cuenta. Si no querés que figure en el mapita porque te da vergüenza (e.g. me recomendás menta granizada o quinotos al whisky), todo bien.
+
+            En lo posible dejame un link a Google Maps, Open Street Map, etc.! 🙏
+        """,
+        "4_form_name": "Nombre (hacete cargo)",
+        "4_form_shop": "Heladería *",
+        "4_form_maps": "Google Maps link",
+        "4_form_what": "Qué me recomendás de este lugar? *",
+        "4_form_add": "Lo agrego al mapita de recomendaciones?",
+        "4_form_add_yes": "Sí",
+        "4_form_add_yes_sub": "Es una recomendación para el mundo entero! 🤗",
+        "4_form_add_no": "No",
+        "4_form_add_no_sub": "Me van a funar 🙈",
+        "4_form_button": "Enviar"
+
+    },
+    "en": {
+        # main page
+        #"0_title": "Heladito map",
+        "0_subh": "Tracking ice creams I try here and there.",
+        "0_text": """
+            Hi there! I built this little app to visualise the ice cream shops and
+            flavours I try around town, like a good #GordaHeladitos.
+
+            If you'd like to recommend me some shop, fill the form at
+            <a href="{app_path}/{page}" target="_self">Recomendame</a>
+            and I'll add your suggestion to my map. Thanks! 🙌""",
+        "0_imagecapt": "My dream: a giant ice cream cone! 🤩. Massera somewhere on the Atlantic coast, 15th February 1995.",
+
+        # map page
+        "1_title": "Map",
+        "1_maintext": """
+            Click on the marker to see the shop's average score.
+            The icons are colour-coded according to average score and my ice cream standards:  
+            🟥: just terrible  
+            🟧: I'd rather eat a cheap ice lolly  
+            🟦: pretty good, if I'm in the area I'll take a 1/4 kg  
+            🟩: inject this into my veinsss
+        """,
+        "1_cityopt": "City",
+        "1_show": "Show",
+        "1_showopt1": "All",
+        "1_showopt2": "Visited",
+        "1_showopt3": "Recommended",
+
+        # list page
+        "2_title": "List",
+        "2_subh1": "Shops",
+        "2_text": "Select a shop to see what flavours I got and what I thought in general.",
+        "2_cityselect": "City",
+        "2_shopselect": "Shop",
+        "2_shopavg": "Average",
+        "2_notes": "Notes",
+        "2_subh2": "Full list",
+        "2_tabletitle": "See table",
+
+        # stats page
+        "3_text1": "What are my favourite flavours? Which ones do I order the most? The answer won't surprise you.",
+        "3_yearselect": "Select year:",
+        "3_yearselect1": "All",
+        "3_text2": "How many shops (aggregated) did I go to and how many different flavours did I try?",
+        "3_nshops": "N° shops",
+        "3_nflavours": "N° flavours",
+        "3_rank_title": "Flavour ranking",
+        "3_rank_text": "Which ones were my favourites? Random order if scores are equal.",
+        "3_scoredistr": "Score distribution",
+        "3_scoredistr_text": "From 1 to 5",
+        "3_creamsorbet": "Cream based or sorbet?",
+        "3_category": "Category",
+        "3_category_text": "Typical categories.",
+        "3_baseflavour": "Base flavour",
+        "3_baseflavour_text": "The flavour with highest percentage of the serving. I got some weird ones only once so I'm only showing the top 15.",
+        "3_extrastoppings": "Extra thingies on top or in the serving",
+        "3_extrastoppings_text": "Some ma have been mixed. Top 10 only!",
+
+        # recommend page
+        "4_title": "Recommend me something!",
+        "4_maintext": """
+            If you know any shop/flavours that I should try, please fill this form and I'll keep it in mind.
+            If you don't want it to be on the map because you're ashamed (e.g. you recommend chocolate mint or whisky kumquats),
+            it's all good.
+
+            If possible leave a link to Google Maps, Open Street Map, etc.! 🙏
+        """,
+        "4_form_name": "Name (own it)",
+        "4_form_shop": "Shop name *",
+        "4_form_maps": "Google Maps link",
+        "4_form_what": "What flavour do you recommend from this place? *",
+        "4_form_add": "Shall I add it to the map?",
+        "4_form_add_yes": "Yes",
+        "4_form_add_yes_sub": "It's a recommendation for the whole world! 🤗",
+        "4_form_add_no": "No",
+        "4_form_add_no_sub": "I don't want to be cancelled 🙈",
+        "4_form_button": "Submit"
+    }
+}
