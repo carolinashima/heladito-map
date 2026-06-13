3ab90221c030c5a75a28aefde03d5ff525f77ae4
carolinashima <shima.carolina@gmail.com>
2026-06-13 22:40:05 +0200

    add multilang

--- pages/1_Mapa.py
+++ pages/1_Mapa.py
@@ -4,14 +4,10 @@ import folium
 from streamlit_folium import st_folium
 import pandas as pd
 from st_social_media_links import SocialMediaIcons
+from translations import translations
+lang = st.session_state.get("lang", "es")
+t = translations[lang]
 
-# page config
-st.set_page_config(layout='wide',
-initial_sidebar_state='expanded',
-page_title='Heladito map',
-page_icon='🍦')
-
-st.sidebar.header('🍦 Heladito map')
 st.sidebar.markdown('''
 ---
 Made by Carolina L. Shimabukuro 👩🏻‍💻
@@ -25,14 +21,9 @@ social_media_icons = SocialMediaIcons(social_media_links, colors)
 
 social_media_icons.render(sidebar=True, justify_content='start')
 
-st.header('Mapa')
-st.markdown('''
-Hacé click en el marker para ver el puntaje promedio de la heladería.
-Los íconos están color-coded según el puntaje promedio y mis estándares heladeriles:  
-🟥: simplemente una gaver  
-🟧: preferiría clavar un Torpedo en el kiosco  
-🟦: bastante bien, si estoy de paso me pido un cuarto  
-🟩: inyéctenlo en mis venasss
+st.header(t["1_title"])
+st.markdown(f'''
+            {t["1_maintext"]}
 ''')
 
 # load data from session state
@@ -42,7 +33,7 @@ center_baires = [-34.51503743529771, -58.489184953954116]
 center_berlin = [52.50043422330395, 13.406035370951068]
 
 # create map
-center_point = st.radio("Ciudad", ["Buenos Aires","Berlin"])
+center_point = st.radio(t["1_cityopt"], ["Buenos Aires","Berlin"])
 if center_point == "Buenos Aires":
     ciudad = center_baires
 elif center_point == "Berlin":
@@ -50,14 +41,13 @@ elif center_point == "Berlin":
 map = folium.Map(location = ciudad, zoom_start = 12)
 
 # choose what to show
-display_options = ["Todos","Visitados","Recomendados"]
-#selection = st.pills("Mostrar:", display_options, selection_mode="multi")
+display_options = [t["1_showopt1"],t["1_showopt2"],t["1_showopt3"]]
 selection = st.radio(
-    "Mostrar",
-    ["Todos","Visitados","Recomendados"]
+    t["1_show"],
+    [t["1_showopt1"],t["1_showopt2"],t["1_showopt3"]]
 )
 
-if selection == "Todos":
+if selection == t["1_showopt1"]:
     for _, row in df_recs.iterrows():
         location = pd.to_numeric(row['latitude']), pd.to_numeric(row['longitude'])
         folium.Marker(location, 
@@ -81,7 +71,7 @@ if selection == "Todos":
         icon=folium.Icon(icon='fa-ice-cream',prefix='fa',color=color)).add_to(map)
 
     
-if selection == "Visitados":
+if selection == t["1_showopt2"]:
     for _, row in df_shops.iterrows():
         location = pd.to_numeric(row['latitude']), pd.to_numeric(row['longitude'])
         if row['avg'] < 2:
@@ -97,7 +87,7 @@ if selection == "Visitados":
         tooltip=row['nombre'],
         icon=folium.Icon(icon='fa-ice-cream',prefix='fa',color=color)).add_to(map)
 
-elif selection == "Recomendados":
+elif selection == t["1_showopt3"]:
     for _, row in df_recs.iterrows():
         location = pd.to_numeric(row['latitude']), pd.to_numeric(row['longitude'])
         folium.Marker(location, 
