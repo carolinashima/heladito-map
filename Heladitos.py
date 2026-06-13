8d35d525d6bc6ba627793d70b1fb19ace161a037
carolinashima <shima.carolina@gmail.com>
2026-06-13 22:40:01 +0200

    add multilang

--- Heladitos.py
+++ Heladitos.py
@@ -3,14 +3,10 @@ import folium
 from streamlit_folium import st_folium
 from streamlit_gsheets import GSheetsConnection
 from st_social_media_links import SocialMediaIcons
+from translations import translations
+lang = st.session_state.get("lang", "es")
+t = translations[lang]
 
-# page config
-st.set_page_config(layout='wide',
-initial_sidebar_state='expanded',
-page_title='Heladito map',
-page_icon='🍦')
-
-st.sidebar.header('🍦 Heladito map')
 st.sidebar.markdown('''
 ---
 Made by Carolina L. Shimabukuro 👩🏻‍💻
@@ -23,13 +19,7 @@ colors = ["black","black"]
 social_media_icons = SocialMediaIcons(social_media_links, colors)
 social_media_icons.render(sidebar=True, justify_content='start')
 
-# landing page content
-st.title('''
-Heladito map 🍦
-''')
-st.subheader('''
-Tracker de helados que voy probando por ahí.
-''')
+st.subheader(t["0_subh"])
 
 app_path = 'https://heladito-map.streamlit.app/'
 page_file_path = 'pages/Recomendame.py'
@@ -40,18 +30,12 @@ col1, col2 = st.columns([0.7, 0.3], gap = 'small')
 
 with col1:
     st.markdown(f'''
-    Hola! Hice esta pequeña app para visualizar mejor las heladerías
-    y helados que voy probando por ahí, como buena #GordaHeladitos.
-
-    Si tenés ganas de recomendarme alguna heladería llená el formulario en
-    <a href="{app_path}/{page}" target="_self">Recomendame</a>
-    y agregaré tu sugerencia a mi mapita. Gracias! 🙌
+        {t["0_text"]}
     ''',
     unsafe_allow_html=True)
 
 with col2:
-    st.image(".static/massera.png",
-    "El sueño de la piba: helado gigante 🤩. Massera en algún lugar de la costa atlántica, 15 de febrero de 1995.")
+    st.image(".static/massera.png", t["0_imagecapt"])
 
 # load data
 st.spinner("Connecting to Google Sheets...")
